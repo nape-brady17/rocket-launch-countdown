5d9d38f444cb91ea5382fa56521f49429d251cb0
Brady <bcnapier5@yahoo.com>
2025-07-09 21:08:32 -0400

    switched to using lldev for API call

--- src/backend/data.ts
+++ src/backend/data.ts
@@ -6,11 +6,11 @@ export interface Launch {   // this interface can and should be expanded to incl
 }
 
 // both of the calls limit the number of responses to 5 for simpler handling for now (this can be exapanded later)
-// these links may need to be updated, I am currently getting rate limits
+// these links uses lldev for development (do not contain rate limits), will need to switch to ll for development
 
 export async function fetchUpcomingLaunches() {
   try {
-    const res = await fetch('https://ll.thespacedevs.com/2.2.0/launch/upcoming/?location__ids=27,12&status__ids=1,8&limit=5', {
+    const res = await fetch('https://lldev.thespacedevs.com/2.2.0/launch/upcoming/?location__ids=27,12&status__ids=1,8&limit=5', {
       mode: 'cors',
     });
     const data: any = await res.json();
@@ -30,7 +30,7 @@ export async function fetchUpcomingLaunches() {
 
 export async function fetchPreviousLaunches() {
   try {
-    const res = await fetch('https://ll.thespacedevs.com/2.2.0/launch/previous/?location__ids=27,12&limit=5', {
+    const res = await fetch('https://lldev.thespacedevs.com/2.2.0/launch/previous/?location__ids=27,12&limit=5', {
       mode: 'cors',
     });
     const data: any = await res.json();
